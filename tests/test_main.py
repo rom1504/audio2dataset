43b0e915ea30f610fd1f643f347273fb125ec7e3
Romain Beaumont <romain.rom1@gmail.com>
2022-05-15 22:15:10 +0200

    fix test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,7 @@
 import pytest
-from audio2dataset import hello_world
+from audio2dataset import download
 
 
 @pytest.mark.parametrize("message", ["hello", "world"])
 def test_hello_world(message):
-    hello_world(message)
+    print("hello")
